09959cdc94290d682b9c25fc19dc2d24fd3752bb
Martin Šulc <martin.sulc2551@gmail.com>
2023-11-22 10:21:03 +0100

    Prep backendu

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import cors from 'cors';
+import express, { Express, Request, Response } from 'express';
+import router from './router';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+
+const tokenFilePath = '../back-end/token.json';
+const app: Express = express();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Calendar Backend API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.ts'], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(4000, () => {
+  console.log('Server running on port 4000');
+});
+
+export default app;
