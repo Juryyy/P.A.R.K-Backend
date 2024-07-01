86e5d06d50e250a3fa6e4fe9f09d881c7d3b1563
Martin Šulc <martin.sulc2551@gmail.com>
2024-07-01 09:33:32 +0200

    Availability update

---
+++ prisma/migrations/20240629093642_response_enum/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - The values [Maybe] on the enum `ResponseEnum` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ResponseEnum_new" AS ENUM ('Yes', 'AM', 'PM', 'No');
+ALTER TABLE "Response" ALTER COLUMN "response" DROP DEFAULT;
+ALTER TABLE "Response" ALTER COLUMN "response" TYPE "ResponseEnum_new" USING ("response"::text::"ResponseEnum_new");
+ALTER TYPE "ResponseEnum" RENAME TO "ResponseEnum_old";
+ALTER TYPE "ResponseEnum_new" RENAME TO "ResponseEnum";
+DROP TYPE "ResponseEnum_old";
+ALTER TABLE "Response" ALTER COLUMN "response" SET DEFAULT 'No';
+COMMIT;
